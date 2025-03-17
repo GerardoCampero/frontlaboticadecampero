825e620323ce9290a8e83c7cf9b2c0ac26065dd4
Gerardo <geragrungeeb@gmail.com>
2025-03-17 16:17:55 -0300

    Se agregan variables de entorno

--- src/components/context/context.jsx
+++ src/components/context/context.jsx
@@ -13,33 +13,45 @@ export default function PageContext({ children }) {
     const [cliente, setCliente] = useState(null)
     const [lotes, setLotes] = useState(null)
     const [lote, setLote] = useState(null)
+    const [URL, setURL] = useState(null) // Inicializa como null, ya que es un valor que se obtiene dinámicamente
 
     useEffect(() => {
-        const consultarTodo = async () => {
-            try {
-                const clientesEnd = await axios.get("http://127.0.0.1:8000/consultar_usuarios")
-                setClientes(clientesEnd.data)
-                // const lotesEnd = await axios.get('http://127.0.0.1:8000/consultar_lotes')
-                // setLotes(lotesEnd.data)
-            } catch (error) {
-                console.error("Error al realizar la consulta", error)
-            }
+        // Comprobar el entorno y establecer la URL
+        if (process.env.NEXT_PUBLIC_ENTORNO === 'PRD') {
+            setURL(process.env.NEXT_PUBLIC_URL_PRD)
+        } else {
+            setURL(process.env.NEXT_PUBLIC_URL_DEV)
         }
-        consultarTodo()
-    }, [])
+    }, []) // Solo se ejecuta una vez al montar el componente
 
+    useEffect(() => {
+        // Solo realizar la consulta si la URL está definida
+        console.log(URL,'URL')
+        if (URL) {
+            const consultarTodo = async () => {
+                try {
+                    const clientesEnd = await axios.get(`${URL}/consultar_usuarios`)
+                    setClientes(clientesEnd.data)
+                    // const lotesEnd = await axios.get(`${URL}/consultar_lotes`)
+                    // setLotes(lotesEnd.data)
+                } catch (error) {
+                    console.error("Error al realizar la consulta", error)
+                }
+            }
+            consultarTodo()
+        }
+    }, [URL]) // Solo ejecuta esta parte cuando URL cambie
 
     const handleConsultarTodo = async () => {
         try {
-            const clientesEnd = await axios.get("http://127.0.0.1:8000/consultar_usuarios")
+            const clientesEnd = await axios.get(`${URL}/consultar_usuarios`)
             setClientes(clientesEnd.data)
-            // const lotesEnd = await axios.get('http://127.0.0.1:8000/consultar_lotes')
+            // const lotesEnd = await axios.get(`${URL}/consultar_lotes`)
             // setLotes(lotesEnd.data)
         } catch (error) {
             console.error("Error al realizar la consulta", error)
         }
     }
-
 
     const contexValue = {
         handleConsultarTodo,
@@ -49,17 +61,15 @@ export default function PageContext({ children }) {
         setLotes,
         cliente,
         setCliente,
-        lote, 
+        lote,
         setLote,
-        
-        
     }
+
     console.log(clientes, 'context')
-    return(
+
+    return (
         <store.Provider value={contexValue}>
             {children}
         </store.Provider>
     )
-
 }
-

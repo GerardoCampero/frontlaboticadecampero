20f36860396c98e942cbaf396a7d102a2d456915
Gerardo <geragrungeeb@gmail.com>
2025-04-21 20:00:29 -0300

    fecha modificada

--- src/components/vistas/componentes/tablaLotes.jsx
+++ src/components/vistas/componentes/tablaLotes.jsx
@@ -280,7 +280,7 @@ const EditarLote = ({ id, fecha }) => {
     descripcion: '',
     cantidad: '',
     precio: '',
-    fecha: new Date(),  // Inicializamos la fecha con el valor actual
+    fecha: fecha,  // Inicializamos la fecha con el valor actual
   });
 
   const handleInputChange = (e) => {

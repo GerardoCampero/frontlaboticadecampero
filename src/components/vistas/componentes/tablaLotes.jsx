93e68dc09e775adec1273d3575c8f73c9b46c914
Gerardo <geragrungeeb@gmail.com>
2025-04-21 21:01:33 -0300

    Test

--- src/components/vistas/componentes/tablaLotes.jsx
+++ src/components/vistas/componentes/tablaLotes.jsx
@@ -103,13 +103,14 @@ export default function TablaLotes({ lotes }) {
                   <Text key={index} w={'100%'} textAlign={'center'} borderBottom={'solid'} borderColor={'gray.100'}>
                     {item.usuario_id}
                   </Text>
-                </MenuTrigger>
+                
                 <MenuContent>
                   <VStack>
                     <EditarLote id={item.id} fecha={item.fecha}/>
                     <Button variant={'ghost'} size={'xs'} onClick={() => handleEliminarLote(item.id)}>Eliminar</Button>
                   </VStack>
                 </MenuContent>
+                </MenuTrigger>
               </MenuRoot>
             ))}
           </VStack>
